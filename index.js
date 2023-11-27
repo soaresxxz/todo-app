8095ec27d5e7d0f91370d4f2dc509f55d0cddb0c
unknown <mateus.soares_2013@hotmail.com>
2023-11-27 19:06:16 -0300

    adiçao dos icones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require("express")
+const express = require("express") 
+const exphbs = require("express-handlebars")
+
+
 
 const app = express()
 
+app.engine('handlebars', exphbs.engine)
+app.set('view engine' , 'handlebars')
+
+app.use(express.static('public'))
+
 app.get('/' , ( requisicao , resposta) => {
-    resposta.send("Olá mundo!")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
