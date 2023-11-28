87a42b3dddb1937a40a37c5d1cef03358a6a5e56
unknown <mateus.soares_2013@hotmail.com>
2023-11-27 23:53:16 -0300

    ultimo ajustes tarefas

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express") 
 const exphbs = require("express-handlebars")
 
-
-
 const app = express()
 
-app.engine('handlebars', exphbs.engine)
+app.engine('handlebars', exphbs.engine())
 app.set('view engine' , 'handlebars')
 
 app.use(express.static('public'))
