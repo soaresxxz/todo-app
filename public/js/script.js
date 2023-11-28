4c7eb75416d29b25383c21506308f4517e0041e9
unknown <mateus.soares_2013@hotmail.com>
2023-11-28 10:00:15 -0300

    deixando a mudança de tema dinamica

---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+function alterarTema(){
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        let novoTema
+
+        if (tema === "light"){
+            novoTema = "dark"
+            button.innerHTML = `<img src = "/imagens/sun-icon.png" alt="icone do Sol">`
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else{
+            novoTema = "light"
+            button.innerHTML = `<img src = "/imagens/moon-icon.png" alt="icone da Lua   ">`
+            body.classList.remove("dark")
+            body.classList.add("light")
+           
+        }
+
+        localStorage.setItem("tema" , novoTema)
+        return
+    }
+
+    localStorage.setItem("tema" , "dark")
+    body.classList.add("dark")
+
+
+
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+
+    if (tema){
+        if (tema === "dark"){
+            body.classList.add("dark")
+            button.innerHTML = `<img src = "/imagens/sun.png" alt="icone do Sol">`
+        } else {
+            body.classList.add("light")
+            button.innerHTML = `<img src = "/imagens/moon.png" alt="icone da Lua   ">`
+        }
+    }
+}
+
+verificarTema() 
+
